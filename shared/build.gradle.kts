57728b29d29f11a47300d0b9777c8ed8f3ab6a78
crewcrew3 <143967753+crewcrew3@users.noreply.github.com>
2026-05-05 17:34:22 +0300

    some changes in buld.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -11,8 +11,8 @@ kotlin {
     // See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
     androidLibrary {
         namespace = "ru.itis.bloom.shared"
-        compileSdk = 36
-        minSdk = 26
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
     }
 
     // Desktop (JVM) target
