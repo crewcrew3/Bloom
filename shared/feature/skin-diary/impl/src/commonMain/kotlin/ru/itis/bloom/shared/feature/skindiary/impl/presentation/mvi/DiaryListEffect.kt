2b819478f48f9958742fa290c3bcce284fb4186e
hkkkkjv <hkkkkjv@mail.ru>
2026-06-02 02:21:32 +0300

    complete cleanup of legacy presentation paths

--- shared/feature/skin-diary/impl/src/commonMain/kotlin/ru/itis/bloom/shared/feature/skindiary/impl/presentation/mvi/DiaryListEffect.kt
+++
@@ -1,9 +0,0 @@
-package ru.itis.bloom.shared.feature.skindiary.impl.presentation.mvi
-
-import org.jetbrains.compose.resources.StringResource
-
-sealed interface DiaryListEffect {
-    data class ShowError(val messageRes: StringResource) : DiaryListEffect
-    data object NavigateToCreate : DiaryListEffect
-    data class NavigateToDetail(val entryId: String) : DiaryListEffect
-}
--- shared/feature/skin-diary/impl/src/commonMain/kotlin/ru/itis/bloom/shared/feature/skindiary/impl/presentation/mvi/DiarySortOrder.kt
+++
@@ -1,7 +0,0 @@
-package ru.itis.bloom.shared.feature.skindiary.impl.presentation.mvi
-
-enum class DiarySortOrder(val apiValue: String) {
-    DATE_DESC("date_desc"),
-    DATE_ASC("date_asc"),
-    SKIN_CONDITION_DESC("skin_condition_desc")
-}
